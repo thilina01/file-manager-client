d41a998f532b1c593282b044d658e16e1e90e7b7
thilina01 <r.thilina@gmail.com>
2016-08-12 19:30:44 +0530

    converted to work as SPA in angular
    moved page contents to pages directory
    moved menu bar to menu.html
    moved folder creation form to folder-form.html
    added description column to folder form
    implemented angular routing

--- js/angularApp.js
+++ js/angularApp.js
@@ -1,20 +1,49 @@
-var app = angular.module('app', []);
-app.controller(
-	'myController', function ($scope, $http) {
-		
-		$scope.folders = '';
+var app = angular.module('app', [ 'ngRoute' ]);
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
 
-		$scope.load = function () {
-			
-		$scope.errorMessage='';
-			$http.get(
-				'http://localhost:8080/'
-			).then(
-				function (response) {
-					$scope.folders = response.data;
-					//alert(JSON.stringify(response.data));
-				}
-			);
-		}
+	// route for the home page
+	.when('/', {
+		templateUrl : 'pages/home.html',
+		controller : 'myController'
+	})
+
+	// route for the folder page
+	.when('/folder', {
+		templateUrl : 'pages/folder.html',
+		controller : 'myController'
+	})
+
+});
+
+app.controller('myController', function($scope, $http) {
+
+	$scope.folders = '';
+	$scope.name = '';
+	$scope.description = '';
+
+	$scope.load = function() {
+		$scope.errorMessage = '';
+		$http.get('http://localhost:8080/folders').then(function(response) {
+			$scope.folders = response.data;
+			//alert(JSON.stringify($scope.folders));
+		});
 	}
-);
+
+	$scope.createFolder = function() {
+
+		$http.post("http://localhost:8080/folders", {
+			name : $scope.name,
+			description : $scope.description
+		}, {
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function(response) {
+			$scope.name = '';
+			$scope.load();
+	        $location.path("/");
+			return response;
+		});
+	}
+});
