e8c82ce8a2995797ffbe631bd712c47af5f67f6b
thilina01 <r.thilina@gmail.com>
2016-08-12 23:00:14 +0530

    enhance coding
    introduced seperate controllers for login, register, index, folder,
    folder form, home

--- js/angularApp.js
+++ js/angularApp.js
@@ -1,37 +1,30 @@
 var app = angular.module('app', [ 'ngRoute' ]);
+
 app.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-
-	// route for the home page
-	.when('/', {
+	$routeProvider.when('/', {
 		templateUrl : 'pages/home.html',
-		controller : 'myController'
-	})
-
-	// route for the folder page
-	.when('/folder', {
+		controller : 'homeController'
+	}).when('/folder', {
 		templateUrl : 'pages/folder.html',
-		controller : 'myController'
+		controller : 'folderController'
 	})
-
 });
 
-app.controller('myController', function($scope, $http) {
-
-	$scope.folders = '';
-	$scope.name = '';
-	$scope.description = '';
-
+app.controller('homeController', function($scope, $http) {
+	$scope.folders = "";
 	$scope.load = function() {
 		$scope.errorMessage = '';
 		$http.get('http://localhost:8080/folders').then(function(response) {
 			$scope.folders = response.data;
-			//alert(JSON.stringify($scope.folders));
 		});
 	}
+});
+
+app.controller('folderFormController', function($scope, $http) {
+	$scope.name = '';
+	$scope.description = '';
 
 	$scope.createFolder = function() {
-
 		$http.post("http://localhost:8080/folders", {
 			name : $scope.name,
 			description : $scope.description
@@ -41,9 +34,59 @@ app.controller('myController', function($scope, $http) {
 			}
 		}).then(function(response) {
 			$scope.name = '';
-			$scope.load();
-	        $location.path("/");
+			$scope.description = '';
+			$scope.load(); // this call is important to refresh folder list in index page
 			return response;
 		});
 	}
+});
+
+app.controller('loginFormController', function($scope, $http) {
+	$scope.email = '';
+	$scope.password = '';
+
+	$scope.login = function() {
+		$http.post("http://localhost:8080/login", {
+			email : $scope.email,
+			password : $scope.password
+		}, {
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function(response) {
+			$scope.email = '';
+			$scope.password = '';
+			$scope.load(); // this call is important to refresh folder list in index page
+			return response;
+		});
+	}
+});
+app.controller('registerFormController', function($scope, $http) {
+	$scope.email = '';
+	$scope.password = '';
+	$scope.passwordAgain = '';
+
+	$scope.register = function() {
+		$http.post("http://localhost:8080/register", {
+			email : $scope.email,
+			password : $scope.password
+		}, {
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function(response) {
+			$scope.email = '';
+			$scope.password = '';
+			$scope.passwordAgain = '';
+			$scope.load(); // this call is important to refresh folder list in index page
+			return response;
+		});
+	}
+});
+
+app.controller('indexController', function($scope, $http) {
+
+});
+app.controller('folderController', function($scope, $http) {
+
 });
