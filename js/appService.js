dd5714c161527ab135c475894d51fc1abdbe984f
thilina01 <r.thilina@gmail.com>
2016-08-31 14:42:07 +0530

    New Service to serve application informations
    New Service to serve application informations

---
+++ js/appService.js
@@ -0,0 +1,6 @@
+
+app.service('appService', function () {
+    this.baseURL = 'http://localhost:8080';
+    this.organization = 'TRW Lanka (Pvt) Ltd.';
+    this.appName = 'Document Management System';
+});
