171b18ef9971f75904d508149f67e31549f81218
thilina01 <r.thilina@gmail.com>
2016-08-31 14:44:50 +0530

    folderService added
    folderService added to handle folder related API calls

--- js/folderService.js
+++ js/folderService.js
@@ -1,5 +1,12 @@
 
-app.service('folderService', function () {
+app.service('folderService', function ($http, appService) {
+    var apiURL = appService.baseURL + '/folders/';
+    var jsonHeaders = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
     var id = 0;
     this.setId = function (newObj) {
         id = newObj;
@@ -8,10 +15,18 @@ app.service('folderService', function () {
     this.getId = function () {
         return id;
     };
-    /*
-     return {
-     setId: setId,
-     getId: getId
-     };
-     */
+
+    /* <-----------------> */
+    this.getTopLevelFolders = function () {
+        return $http.get(apiURL + 'top');
+    };
+    this.getFoldersWithParent = function () {
+        return $http.get(apiURL + id + '/with-parent');
+    };
+    this.getFoldersWithSubFoldersAndFiles = function () {
+        return $http.get(apiURL + id + '/with-sub-folders-and-files');
+    };
+    this.createFolder = function (data) {
+        return $http.post(apiURL, data, jsonHeaders);
+    };
 });
