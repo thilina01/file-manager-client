3abcf68c321d0bb5c45426e4de2ecc709b14fbea
thilina01 <r.thilina@gmail.com>
2016-10-03 11:53:46 +0530

    added controller
    added controller for section and wcc forms

---
+++ js/sectionFormController.js
@@ -0,0 +1,5 @@
+
+app.controller('sectionFormController', function ($scope, $cookies, accountService, appService) {
+    $scope.code = '';
+    $scope.name = '';
+});
---
+++ js/workCenterFormController.js
@@ -0,0 +1,6 @@
+
+app.controller('workCenterFormController', function ($scope, $cookies, accountService, appService) {
+    $scope.code = '';
+    $scope.name = '';
+    $scope.section = '';
+});
