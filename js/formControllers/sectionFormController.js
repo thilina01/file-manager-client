e43e3e56736fffc0a69a088492e16f89aa242d76
thilina01 <r.thilina@gmail.com>
2016-10-11 09:53:41 +0530

    added clear and save functions
    added clear and save functions

--- js/formControllers/sectionFormController.js
+++ js/formControllers/sectionFormController.js
@@ -1,5 +1,23 @@
 
-app.controller('sectionFormController', function ($scope, $cookies, accountService, appService) {
+app.controller('sectionFormController', function ($scope, $cookies, sectionService, appService) {
     $scope.code = '';
     $scope.name = '';
+    
+    $scope.clear = function () {
+        alert($scope.code+' '+$scope.name);
+        $scope.code = '';
+        $scope.name = '';
+    }
+    
+    $scope.save = function () {
+        
+        sectionService.save($scope.code, $scope.name).then(function (response) {
+            if (response.data) {
+                alert(response.data);
+            }
+            $scope.reloadApp();
+            return response;
+        });
+    }
+    
 });
