d988bcfcdeadb774913edd6b94cc6fb4b750f27c
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-11 09:39:03 +0530

    Added wcc and section attributes
    Added wcc and section attributes in controlpoint form controller

--- js/formControllers/controlPointFormController.js
+++ js/formControllers/controlPointFormController.js
@@ -2,5 +2,7 @@
 app.controller('breakdownFormController', function ($scope, $cookies, accountService, appService) {
     $scope.code = '';
     $scope.name = '';
+    $scope.wcc = '';
+    $scope.section = '';
    
 });
