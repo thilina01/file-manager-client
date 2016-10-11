b4e9982ec0e5843df1f08f7be2c121768ecba945
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-11 14:29:16 +0530

    added controller
    added controller for production

--- js/formControllers/productionFormController.js
+++ js/formControllers/productionFormController.js
@@ -1,17 +1,79 @@
 
 app.controller('productionFormController', function ($scope, $cookies, accountService, appService) {
+    //main
+    $scope.date = '';
+    $scope.shift = '';
+    $scope.controlPointCode = '';
+    //auto 
+    $scope.controlPointName = '';
+    //production
+    $scope.productionJobNo = '';
+    $scope.productionQuantity = '';
+    //Quality
+    $scope.qualityJobNo = '';
+    $scope.reason = '';
     $scope.code = '';
-    $scope.type = '';
-    $scope.description = '';
+    $scope.qualityQuantity = '';
+    //maintenance
+    $scope.maintenanceJobNo = '';
+    $scope.machineNo = '';
+    $scope.noOfBreakdown = '';
+    $scope.machinerunningTime = '';
+    //Hr
+    $scope.hrJobNo = '';
+    $scope.company = '';
+    $scope.contract = '';
+    $scope.help = '';
 
-
+    $scope.other = '';
 
 
     $scope.clear = function () {
         // alert($scope.code + ' ' + $scope.name);
+        //main
+        $scope.date = '';
+        $scope.shift = '';
+        $scope.controlPointCode = '';
+        //auto 
+        $scope.controlPointName = '';
+        //production
+        $scope.productionJobNo = '';
+        $scope.productionQuantity = '';
+        //Quality
+        $scope.qualityJobNo = '';
+        $scope.reason = '';
         $scope.code = '';
-        $scope.type = '';
-        $scope.description = '';
+        $scope.qualityQuantity = '';
+        //maintenance
+        $scope.maintenanceJobNo = '';
+        $scope.machineNo = '';
+        $scope.noOfBreakdown = '';
+        $scope.machinerunningTime = '';
+        //Hr
+        $scope.hrJobNo = '';
+        $scope.company = '';
+        $scope.contract = '';
+        $scope.help = '';
 
+        $scope.other = '';
+    }
+
+    $scope.save = function () {
+        workCenterService.save($scope.date, $scope.shift, $scope.controlPointCode).then(
+                function (response) {
+                    if (response.data) {
+                        alert(response.data);
+                    }
+                    $scope.reloadApp();
+                    return response;
+                },
+                function (response) {
+                    if (response.data) {
+                        alert(response.data);
+                    }
+                    //$scope.reloadApp();
+                    return response;
+                }
+        );
     }
 });
