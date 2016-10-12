1afdd8502103b969a406927393bf0a5928ef3d64
thilina01 <r.thilina@gmail.com>
2016-10-12 18:22:49 +0530

    Section Grid Data load error fixed
    Section Grid Data load error fixed

--- js/controllers/sectionGridController.js
+++ js/controllers/sectionGridController.js
@@ -2,10 +2,30 @@
 app.controller('sectionGridController', function ($http, $scope, $cookies, sectionService, appService) {
 
     $scope.sections = '';
-
     $scope.loadSections = function () {
         sectionService.getAll().then(function (response) {
             $scope.sections = response.data;
+
+            /*
+             var result = [];
+             for (var i in $scope.sections)
+             result.push([$scope.sections [i].code, $scope.sections [i].name]);
+             alert(result);*/
+
+            var x = $('#sectionTable').DataTable({
+                data: $scope.sections,
+                columns: [
+                    {data: 'code'},
+                    {data: 'name'}
+                ]
+            });
         });
     }
+
+    $('#sectionGridModal').on('shown.bs.modal', function () {
+        $scope.loadSections();
+    })
+    $('#sectionGridModal').on('hidden.bs.modal', function () {
+        $('#sectionTable').DataTable().destroy();
+    })
 });
