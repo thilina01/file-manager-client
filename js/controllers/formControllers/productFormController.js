8709d9e3dd669b6893c9bb510e404e0b46ab45eb
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-13 15:31:32 +0530

    code formatting

--- js/controllers/formControllers/productFormController.js
+++ js/controllers/formControllers/productFormController.js
@@ -1,41 +1,46 @@
 
 app.controller('productFormController', function ($scope, $cookies, accountService, appService, productService) {
-$scope.code = '';
-        $scope.type = '';
-        $scope.description = '';
-        $scope.clear = function () {
+    $scope.code = '';
+    $scope.productType = '';
+    $scope.description = '';
+
+
+
+
+    $scope.clear = function () {
         // alert($scope.code + ' ' + $scope.name);
         $scope.code = '';
-                $scope.type = '';
-                $scope.description = '';
+        $scope.productType = '';
+        $scope.description = '';
+    }
+    $scope.isValid = function () {
+        if ($scope.code == '' || $scope.productType == '' || $scope.description == '') {
+            return false;
         }
-$scope.isValid = function () {
-if ($scope.code == '' || $scope.type == '' || $scope.description == '') {
-return false;
-}
-return true;
-}
+        return true;
+    }
 
-$scope.save = function () {
-if (!$scope.isValid()) {
-$scope.showError("form not complete");
-        return;
-}
-productService.save($scope.code, $scope.type, $scope.description).then(
-        function (response) {
-        if (response.data) {
-        alert(response.data);
-                $scope.clear();
-                $scope.reloadApp();
-                return response;
-        },
+    $scope.save = function () {
+        if (!$scope.isValid()) {
+            $scope.showError("form not complete");
+            return;
+        }
+        productService.save($scope.code, $scope.productType, $scope.description).then(
                 function (response) {
-                if (response.data) {
-                alert(response.data);
-                }
-                //$scope.reloadApp();
-                return response;
+                    if (response.data) {
+                        alert(response.data);
+                    }
+                    $scope.clear();
+                    $scope.reloadApp();
+                    return response;
+                },
+                function (response) {
+                    if (response.data) {
+                        alert(response.data);
+                    }
+                    //$scope.reloadApp();
+                    return response;
                 }
         );
-        }
+    }
 });
