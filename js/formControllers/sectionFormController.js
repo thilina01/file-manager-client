08fd44e4b8eedd5d2664929558111d95b92f0f42
thilina01 <r.thilina@gmail.com>
2016-10-13 08:35:36 +0530

    added validation

--- js/formControllers/sectionFormController.js
+++ js/formControllers/sectionFormController.js
@@ -8,19 +8,30 @@ app.controller('sectionFormController', function ($scope, $cookies, sectionServi
         $scope.name = '';
     }
 
+    $scope.isValid = function () {
+        if ($scope.code == '' || $scope.name == '') {
+            return false;
+        }
+        return true;
+    }
+
     $scope.save = function () {
+        if (!$scope.isValid()) {
+            $scope.showError("form not complete");
+            return;
+        }
         sectionService.save($scope.code, $scope.name).then(
                 function (response) {
                     if (response.data) {
-                        alert(response.data);
+                        $scope.showSuccess("saved");
                     }
                     $scope.reloadApp();
                     return response;
                 },
                 function (response) {
                     if (response.data) {
-                        alert(response.data);
                     }
+                    $scope.showError("Save faild");
                     //$scope.reloadApp();
                     return response;
                 }
