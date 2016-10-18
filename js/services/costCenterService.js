38c08164e8e3efc838b11a7e3f254e8c74c3d189
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-18 15:09:15 +0530

    added reference variable

--- js/services/costCenterService.js
+++ js/services/costCenterService.js
@@ -2,10 +2,11 @@
 app.service('costCenterService', function ($http, appService) {
     var apiURL = appService.baseURL + 'costCenters/';
 
-    this.save = function (code, name) {
+    this.save = function (code, name, section) {
         var data = {
             code: code,
-            name: name
+            name: name,
+            section: {id: section.id}
         };
         return  $http.post(apiURL, data, appService.getJsonHeaders());
     };
