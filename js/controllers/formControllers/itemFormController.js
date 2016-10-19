4862d406c46d76593f36f8f78ce2d8b68885415e
thilina01 <r.thilina@gmail.com>
2016-10-19 20:06:43 +0530

    Problems with Item Form and Controller fixed
    Problems with Item Form and Controller fixed
    Loading Paints implemented

--- js/controllers/formControllers/itemFormController.js
+++ js/controllers/formControllers/itemFormController.js
@@ -1,54 +1,63 @@
 
-app.controller('itemFormController', function ($scope, $cookies, accountService, appService, itemService) {
+app.controller('itemFormController', function ($scope, $cookies, accountService, appService, itemService, itemTypeService, paintService) {
+    $scope.item = {};
+    $scope.itemType = {};
+    $scope.paint = {};
+
+    $scope.itemTypes = [];
+    $scope.paints = [];
+
+    $scope.loadItemType = function () {
+        itemTypeService.getAll().then(function (response) {
+            $scope.itemTypes = response.data;
+        });
+
+    }
+    $scope.loadPaints = function () {
+        paintService.getAll().then(function (response) {
+            $scope.paints = response.data;
+        });
+
+    }
+    $scope.clear = function () {
         $scope.item = {};
         $scope.itemType = {};
-        $scope.itemTypes = [];
-        
-        $scope.loadItemType = function () {
-        itemTypeService.getAll().then(function (response) {
-        $scope.itemTypes = response.data;
-        });
-        
-        }
-                $scope.clear = function () {
-                // alert($scope.code + ' ' + $scope.name);
-                        $scope.item = {};
-                        $scope.itemType = {};
-                }
-        $scope.isValid = function () {
-        if ($scope.item.code == '' || angular.equals($scope.itemType, {}) || $scope.item.description == '') {
-        return false;
+        $scope.paint = {};
+    }
+    $scope.isValid = function () {
+        if ($scope.item.code == '' || $scope.item.description == '' || angular.equals($scope.itemType, {}) || angular.equals($scope.paint, {})) {
+            return false;
         }
         return true;
-        }
+    }
 
-        $scope.save = function () {
+    $scope.save = function () {
         if (!$scope.isValid()) {
-        $scope.showError("form not complete");
-                return;
+            $scope.showError("form not complete");
+            return;
         }
         $scope.item.itemType = JSON.parse($scope.itemType);
-        itemService.save($scope.itemType).then(
+        $scope.item.paint = JSON.parse($scope.paint);
+        itemService.save($scope.item).then(
                 function (response) {
-                if (response.data) {
-
-                $scope.showSuccess("saved");
-                }
-                $scope.clear();
-                        $scope.reloadApp();
-                        return response;
+                    if (response.data) {
+                        $scope.showSuccess("saved");
+                    }
+                    $scope.clear();
+                    $scope.reloadApp();
+                    return response;
                 },
                 function (response) {
-                if (response.data) {
-                //alert(response.data);
-                }
-
-                $scope.showError("Save faild");
-                        return response;
+                    if (response.data) {
+                        //alert(response.data);
+                    }
+                    $scope.showError("Save faild");
+                    return response;
                 }
         );
-        }
-         $('#itemModal').on('shown.bs.modal', function () {
+    }
+    $('#itemModal').on('shown.bs.modal', function () {
         $scope.loadItemType();
+        $scope.loadPaints();
     })
-        });
+});
