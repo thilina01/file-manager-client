2cbbe5126ca66045096cf6cf004099afa2a17076
thilina01 <r.thilina@gmail.com>
2016-10-19 20:07:04 +0530

    Paint Service
    Created and linked

---
+++ js/services/paintService.js
@@ -0,0 +1,13 @@
+
+app.service('paintService', function ($http, appService) {
+    var apiURL = appService.baseURL + 'paints/';
+
+    this.save = function (paint) {
+        return  $http.post(apiURL, paint, appService.getJsonHeaders());
+    };
+
+
+    this.getAll = function () {
+        return $http.get(apiURL, appService.getJsonHeaders());
+    };
+});
