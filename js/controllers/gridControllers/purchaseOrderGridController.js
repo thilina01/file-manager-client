a1b999971129eaf8097ed131b3e6bd104c2390ed
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-20 15:46:43 +0530

    create purchase order grid and grid controller

---
+++ js/controllers/gridControllers/purchaseOrderGridController.js
@@ -0,0 +1,20 @@
+
+app.controller('purchaseOrderGridController', function ($http, $scope, $cookies, purchaseOrderService, appService) {
+
+    $scope.purchaseOrders = '';
+
+    $scope.loadPurchaseOrders = function () {
+       purchaseOrderService.getAll().then(function (response) {
+            $scope.purchaseOrders = response.data;
+
+           
+        });
+    }
+
+    $('#jobGridModal').on('shown.bs.modal', function () {
+        $scope.loadpurchaseOrders();
+    })
+    $('#purchaseOrderGridModal').on('hidden.bs.modal', function () {
+        $('#purchaseOrderTable').DataTable().destroy();
+    })
+});
