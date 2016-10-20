fb24b3d3d4fc0864355d47e460ad3e8f5e01389f
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-20 15:47:53 +0530

    modified job grid and grid controller

--- js/controllers/gridControllers/jobGridController.js
+++ js/controllers/gridControllers/jobGridController.js
@@ -7,23 +7,12 @@ app.controller('jobGridController', function ($http, $scope, $cookies, jobServic
         jobService.getAll().then(function (response) {
             $scope.jobs = response.data;
 
-            var x = $('#jobTable').DataTable({
-                data: $scope.jobs,
-                columns: [
-                    {data: 'jobNo'},
-                    {data: 'productCode'},
-                    {data: 'productType'},
-                    {data: 'customer'},
-                    {data: 'customerCode'},
-                    {data: 'itemDescription'},
-                    {data: 'jobQty'},
-                ]
-            });
+            
         });
     }
 
     $('#jobGridModal').on('shown.bs.modal', function () {
-        $scope.loadcustomers();
+        $scope.loadjobs();
     })
     $('#jobGridModal').on('hidden.bs.modal', function () {
         $('#jobTable').DataTable().destroy();
