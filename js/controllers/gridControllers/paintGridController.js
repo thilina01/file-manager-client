2d703f38ca5a48c1288ded01eb15404e9d6f2912
Daminda <daminda4x@gmail.com>
2016-10-25 12:23:06 +0530

    Datatable Related improvements

--- js/controllers/gridControllers/paintGridController.js
+++ js/controllers/gridControllers/paintGridController.js
@@ -1,27 +1,48 @@
+app.controller('paintGridController', function ($http, $scope, $cookies, paintService, dataTableService, appService) {
 
-app.controller('paintGridController', function ($http, $scope, $cookies, paintService, appService) {
+    $scope.edit = function () {
+        if ($scope.dataTable.row('.selected').data() != undefined) {
+            paintService.toEdit = $scope.dataTable.row('.selected').data();
+            $('#paintGridModal').modal('hide');
+            $('#paintModal').modal('show');
+
+        }
+    }
+    $scope.delete = function () {
+        if ($scope.dataTable.row('.selected').data() != undefined) {
+            paintService.delete($scope.dataTable.row('.selected').data().id).then(
+                    function (response) {
+                        $scope.dataTable.row('.selected').remove().draw(false);
+                    });
+        }
+    }
 
     $scope.paints = '';
-
-    $scope.loadPaints = function () {
-        paintService.getAll().then(function (response) {
-            $scope.paints = response.data;
-
-            var x = $('#paintTable').DataTable({
-                data: $scope.paints,
+    $scope.table = $('#paintTable');
+    $scope.dataTable = $scope.table.DataTable({
                 columns: [
                     {data: 'code'},
                     {data: 'description'},
                    
-                ]
-            });
+                ],
+        dom: 'Bfrtip',
+        buttons: dataTableService.getButtons($scope.edit, $scope.delete)
+    });
+    $scope.loadPaints = function () {
+        $scope.dataTable.clear();
+        paintService.getAll().then(function (response) {
+            $scope.paints = response.data;
+            $scope.dataTable.rows.add($scope.paints).draw();
         });
     }
+
+    $scope.table.on('click', 'tr', dataTableService.getRowSelector($scope.dataTable));
 
     $('#paintGridModal').on('shown.bs.modal', function () {
         $scope.loadPaints();
     })
     $('#paintGridModal').on('hidden.bs.modal', function () {
-        $('#paintTable').DataTable().destroy();
+       
     })
+
 });
--- js/controllers/gridControllers/purchaseOrderGridController.js
+++ js/controllers/gridControllers/purchaseOrderGridController.js
@@ -1,29 +1,50 @@
+app.controller('purchaseOrderGridController', function ($http, $scope, $cookies, purchaseOrderService, dataTableService, appService) {
 
-app.controller('purchaseOrderGridController', function ($http, $scope, $cookies, purchaseOrderService, appService) {
+    $scope.edit = function () {
+        if ($scope.dataTable.row('.selected').data() != undefined) {
+            purchaseOrderService.toEdit = $scope.dataTable.row('.selected').data();
+            $('#purchaseOrderGridModal').modal('hide');
+            $('#purchaseOrderModal').modal('show');
+
+        }
+    }
+    $scope.delete = function () {
+        if ($scope.dataTable.row('.selected').data() != undefined) {
+            purchaseOrderService.delete($scope.dataTable.row('.selected').data().id).then(
+                    function (response) {
+                        $scope.dataTable.row('.selected').remove().draw(false);
+                    });
+        }
+    }
 
     $scope.purchaseOrders = '';
-
+    $scope.table = $('#purchaseOrderTable');
+    $scope.dataTable = $scope.table.DataTable({
+        columns: [
+            {data: 'poNumber'},
+            {data: 'orderReceivedDate'},
+            {data: 'actualDispatchedDate'},
+            {data: 'purchaseOrderType.type'},
+            {data: 'customer.name'}
+        ],
+        dom: 'Bfrtip',
+        buttons: dataTableService.getButtons($scope.edit, $scope.delete)
+    });
     $scope.loadPurchaseOrders = function () {
+        $scope.dataTable.clear();
         purchaseOrderService.getAll().then(function (response) {
             $scope.purchaseOrders = response.data;
-            var x = $('#purchaseOrderTable').DataTable({
-                data: $scope.purchaseOrders,
-                columns: [
-                    {data: 'poNumber'},
-                    {data: 'orderReceivedDate'},
-                    {data: 'actualDispatchedDate'},
-                    {data: 'purchaseOrderType.type'},
-                    {data: 'customer.name'}
-                ]
-            });
-
+            $scope.dataTable.rows.add($scope.purchaseOrders).draw();
         });
     }
+
+    $scope.table.on('click', 'tr', dataTableService.getRowSelector($scope.dataTable));
 
     $('#purchaseOrderGridModal').on('shown.bs.modal', function () {
         $scope.loadPurchaseOrders();
     })
     $('#purchaseOrderGridModal').on('hidden.bs.modal', function () {
-        $('#purchaseOrderTable').DataTable().destroy();
+
     })
+
 });
