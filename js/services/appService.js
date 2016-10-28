05bc79322fbd45c4232ba4b8261e8f2eb604e153
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-10-28 08:53:54 +0530

    create attendance chart and chart controller

--- js/services/appService.js
+++ js/services/appService.js
@@ -1,7 +1,7 @@
 
 app.service('appService', function ($cookies) {
-    this.baseURL = 'http://192.168.1.98:8080/';//http://trw.nanosl.com:82///tmsapi.trwlanka.com
-    this.clientBaseURL = 'http://tms.trwlanka.com/';//'http://trw.nanosl.com:81/'//http://localhost/file-manager-client
+    this.baseURL = 'http://192.168.1.99:8080/';//http://trw.nanosl.com:82///tmsapi.trwlanka.com
+    this.clientBaseURL = 'http://localhost/file-manager-client';//'http://trw.nanosl.com:81/'//http://localhost/file-manager-client
     this.organization = 'TRW Lanka (Pvt) Ltd.';
     this.appName = 'TRW Management System';
     this.email = $cookies.get("email") != undefined ? $cookies.get("email") : "";
