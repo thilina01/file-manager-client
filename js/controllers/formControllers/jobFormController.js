85195b907c9dc3b5fed405040e406bee4ffbbb7f
thilina01 <r.thilina@gmail.com>
2016-10-28 14:11:52 +0530

    Added job Type selector

--- js/controllers/formControllers/jobFormController.js
+++ js/controllers/formControllers/jobFormController.js
@@ -1,33 +1,32 @@
 
-app.controller('jobFormController', function ($scope,$timeout, $cookies, accountService, appService, jobService, itemService, customerService) {
+app.controller('jobFormController', function ($scope, $timeout, $cookies, accountService, appService, jobService, itemService, jobTypeService) {
     $scope.job = {};
-    $scope.item = {};
-    $scope.customer = {};
 
     $scope.items = [];
-    $scope.customers = [];
-     $scope.saveButtonText = 'Save';
-     
+    $scope.jobTypes = [];
+    $scope.saveButtonText = 'Save';
+
     $scope.loadItems = function () {
         itemService.getAll().then(function (response) {
             $scope.items = response.data;
         });
     }
-    $scope.loadCustomers = function () {
-        customerService.getAll().then(function (response) {
-            $scope.customers = response.data;
+    
+    $scope.loadJobTypes = function () {
+        jobTypeService.getAll().then(function (response) {
+            $scope.jobTypes = response.data;
         });
     }
+    
     $scope.clear = function () {
         // alert($scope.code + ' ' + $scope.name);
         $scope.job = {};
-        $scope.item = {};
-        $scope.customer = {};
-         jobService.toEdit = {};
+        jobService.toEdit = {};
         $scope.saveButtonText = 'Save';
     }
+    
     $scope.isValid = function () {
-
+        $scope.job.jobDate = $('#jobDate').val();
         if ($scope.job.jobDate == '' || $scope.job.jobNo == '' || angular.equals($scope.job.item, {}) || angular.equals($scope.job.customer, {}) || $scope.job.jobQty == '') {
             return false;
         }
@@ -35,14 +34,13 @@ app.controller('jobFormController', function ($scope,$timeout, $cookies, account
     }
 
     $scope.save = function () {
-        /*
         if (!$scope.isValid()) {
             $scope.showError("form not complete");
             return;
         }
-       
-        $scope.job.item = JSON.parse($scope.item);
-        $scope.job.customer = JSON.parse($scope.customer); */
+        /*
+         $scope.job.item = JSON.parse($scope.item);
+         $scope.job.customer = JSON.parse($scope.customer); */
         jobService.save($scope.job).then(
                 function (response) {
                     if (response.data) {
@@ -63,19 +61,18 @@ app.controller('jobFormController', function ($scope,$timeout, $cookies, account
                     return response;
                 }
         );
-
-
     }
+    
     $('#jobModal').on('show.bs.modal', function () {
         $scope.loadItems();
-         $scope.loadCustomers();
-         $scope.saveButtonText = 'Save';
+        $scope.loadJobTypes();
+        $scope.saveButtonText = 'Save';
         if (jobService.toEdit.id != undefined) {
             $timeout(function () {
                 $scope.saveButtonText = 'Update';
                 $scope.job = jobService.toEdit;
             }, 500);
         }
-    
+
     })
 });
