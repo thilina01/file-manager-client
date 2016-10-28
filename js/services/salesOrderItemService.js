af74f895e12327e0d25646f42bd053a645f4320e
thilina01 <r.thilina@gmail.com>
2016-10-28 11:06:45 +0530

    Sales Order Item related modifications

--- js/services/salesOrderItemService.js
+++ js/services/salesOrderItemService.js
@@ -1,15 +1,15 @@
 
 app.service('salesOrderItemService', function ($http, appService) {
     var apiURL = appService.baseURL + 'salesOrderItems/';
-this.toEdit = {};
-    this.save = function (salesOrderItem) {       
+    this.toEdit = {};
+    this.save = function (salesOrderItem) {
         return  $http.post(apiURL, salesOrderItem, appService.getJsonHeaders());
     };
 
     this.getAll = function () {
         return $http.get(apiURL, appService.getJsonHeaders());
     };
-     this.delete = function (id) {
+    this.delete = function (id) {
         return  $http.delete(apiURL + id, appService.getJsonHeaders());
     };
 });
