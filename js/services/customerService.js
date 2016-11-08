3cdcf910069adb86d11558977bd7d78117b99200
Thilina Ranathunga <r.thilina@gmail.com>
2016-11-08 18:17:40 +0530

    save many service call added

--- js/services/customerService.js
+++ js/services/customerService.js
@@ -2,14 +2,21 @@
 app.service('customerService', function ($http, appService) {
     var apiURL = appService.baseURL + 'customers/';
     this.toEdit = {};
+    
     this.save = function (customer) {
         return  $http.post(apiURL, customer, appService.getJsonHeaders());
     };
 
+    this.saveMany = function (customers) {
+        return  $http.post(apiURL+'many', customers, appService.getJsonHeaders());
+    };
+    
     this.getAll = function () {
         return $http.get(apiURL, appService.getJsonHeaders());
     };
+    
     this.delete = function (id) {
         return  $http.delete(apiURL + id, appService.getJsonHeaders());
     };
+    
 });
