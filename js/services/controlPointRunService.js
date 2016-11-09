096947333bdc8e05c364d124fa104341ee4e7e49
Thilina Ranathunga <r.thilina@gmail.com>
2016-11-09 16:00:59 +0530

    added saveMany Function

--- js/services/controlPointRunService.js
+++ js/services/controlPointRunService.js
@@ -2,11 +2,15 @@
 app.service('controlPointRunService', function ($http, appService) {
     var apiURL = appService.baseURL + 'controlPointRuns/';
     this.toEdit = {};
+    
     this.save = function (controlPointRun) {
         return  $http.post(apiURL, controlPointRun, appService.getJsonHeaders());
     };
-
-
+    
+    this.saveMany = function (controlPointRuns) {
+        return  $http.post(apiURL + 'many', controlPointRuns, appService.getJsonHeaders());
+    };
+    
     this.getAll = function () {
         return $http.get(apiURL, appService.getJsonHeaders());
     };
--- js/services/jobService.js
+++ js/services/jobService.js
@@ -1,15 +1,21 @@
 
 app.service('jobService', function ($http, appService) {
     var apiURL = appService.baseURL + 'jobs/';
-this.toEdit = {};
-    this.save = function (job) {       
+    this.toEdit = {};
+    
+    this.save = function (job) {
         return  $http.post(apiURL, job, appService.getJsonHeaders());
     };
 
+    this.saveMany = function (jobs) {
+        return  $http.post(apiURL + 'many', jobs, appService.getJsonHeaders());
+    };
+    
     this.getAll = function () {
         return $http.get(apiURL, appService.getJsonHeaders());
     };
-     this.delete = function (id) {
+    
+    this.delete = function (id) {
         return  $http.delete(apiURL + id, appService.getJsonHeaders());
     };
 });
