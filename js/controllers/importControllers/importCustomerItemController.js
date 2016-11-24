0a1933d5f23a00d87298ac9a1962dd2395b10047
chathurikanc <chathurika.n.chandrapala@gmail.com>
2016-11-24 12:42:55 +0530

    Modified import and import customer controller

---
+++ js/controllers/importControllers/importCustomerItemController.js
@@ -0,0 +1,89 @@
+app.controller('importCustomerItemController', function ($scope, $timeout, $cookies, dataTableService, accountService, appService, customerItemService) {
+    $scope.clear = function () {
+        $scope.customerItems = [];
+        $scope.dataTable.clear();
+        $scope.dataTable.rows.add($scope.customerItems).draw();
+    }
+    $scope.save = function () {
+
+        customerItemService.saveMany($scope.customerItems).then(
+                function (response) {
+                    if (response.data) {
+                        $scope.showSuccess("saved");
+                    }
+                    $scope.clear();
+                    $scope.reloadApp();
+                    return response;
+                },
+                function (response) {
+                    if (response.data) {
+                        alert(response.data);
+                    }
+
+                    $scope.showError("Unable to save");
+                    return response;
+                }
+        );
+    }
+    $scope.setFile = function ($file) {
+        //alert($file.name);
+        $scope.aFile = $file;
+        $scope.fileName = $file.name;
+        //$log.info($file);
+
+        // Get The File From The Input
+        //var oFile = $file;
+        var sFilename = $file.name;
+        // Create A File Reader HTML5
+        var reader = new FileReader();
+        // Ready The Event For When A File Gets Selected
+        reader.onload = function (e) {
+            var data = e.target.result;
+
+
+            /* if binary string, read with type 'binary' 
+             var wb = XLSX.read(data, {type: 'binary'});*/
+
+            var cfb = XLS.CFB.read(data, {type: 'binary'});
+            var wb = XLS.parse_xlscfb(cfb);
+            // Loop Over Each Sheet
+            wb.SheetNames.forEach(function (sheetName) {
+                // Obtain The Current Row As CSV
+                //var sCSV = XLS.utils.make_csv(wb.Sheets[sheetName]);
+                var oJS = XLS.utils.sheet_to_row_object_array(wb.Sheets[sheetName]);
+
+                //$("#my_file_output").html(sCSV);
+                $scope.customerItems = oJS;
+                console.log(oJS)
+            });
+
+            for (var i = 0; i < $scope.customerItems.length; i++) {
+                
+                var customer_code = $scope.customerItems[i].customer_code;
+                var item_code = $scope.customerItems[i].item_code;
+                 $scope.customerItems[i].customer = {code: customer_code};
+                $scope.customerItems[i].item = {code: item_code};
+            }
+            $scope.dataTable.rows.add($scope.customerItems).draw();
+        };
+
+        reader.readAsBinaryString($file);
+    }
+
+    $scope.customerItems = [];
+    $scope.table = $('#importCustomerItemTable');
+    $scope.dataTable = $scope.table.DataTable({
+        columns: [
+             {data: 'customer.code'},
+            {data: 'customerPartNo'},
+            {data: 'item.code'}
+
+        ],
+        dom: 'Bfrtip',
+        buttons: dataTableService.getButtons($scope.edit, $scope.delete)
+    });
+
+
+    $('#importCustomerItemModal').on('show.bs.modal', function () {
+    })
+});
--- js/services/customerItemService.js
+++ js/services/customerItemService.js
@@ -1,15 +1,17 @@
 
 app.service('customerItemService', function ($http, appService) {
     var apiURL = appService.baseURL + 'customerItems/';
-this.toEdit = {};
+    this.toEdit = {};
     this.save = function (customerItem) {
         return  $http.post(apiURL, customerItem, appService.getJsonHeaders());
     };
-
+    this.saveMany = function (customerItems) {
+        return  $http.post(apiURL + 'many', customerItems, appService.getJsonHeaders());
+    };
     this.getAll = function () {
         return $http.get(apiURL, appService.getJsonHeaders());
     };
-     this.delete = function (id) {
+    this.delete = function (id) {
         return  $http.delete(apiURL + id, appService.getJsonHeaders());
     };
 });
