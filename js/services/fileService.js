4861492e9400e5e2f33fb1cd83649b15e086191a
Thilina Ranathunga <r.thilina@gmail.com>
2017-01-03 13:40:51 +0530

    Enabled sending headers with request

--- js/services/fileService.js
+++ js/services/fileService.js
@@ -3,7 +3,7 @@ app.service('fileService', function ($http, appService) {
     var apiURL = appService.baseURL + '/files/';
 
     this.deleteFile = function (id) {
-        return $http.delete(apiURL + id);
+        return $http.delete(apiURL + id, appService.getJsonHeaders());
     };
     this.uploadFile = function (formdata) {
         return $http.post(apiURL + "upload", formdata, appService.getUndefinedHeaders());
